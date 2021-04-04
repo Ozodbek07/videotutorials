53ebac34e868ac29130b78b63034f71eb8dd7200
Ozodbek <ozodbekbaxronovuz@gmail.com>
2021-04-04 22:14:23 +0500

    a

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-window.addEventListener('DOMContentLoaded', function () {
-//reveal
-  window.addEventListener('scroll', () => {
-    let reveals = document.querySelectorAll('.reveal')
-
-    for(let i = 0; i< reveals.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals[i].getBoundingClientRect().top;
-      let revealPoint = 50;
-
-      if(revealTop < windowHeight - revealPoint) {
-        reveals[i].classList.add('active')
-      }
-    }
-  })
-})
 // loader
 window.onload = function () {
   document.body.classList.add('loaded_hiding');
